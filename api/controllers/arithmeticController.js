247a3ae1cd87eeb30f64972cece22a50ada69624
jonrynn <jonrynn@gmail.com>
2020-06-23 16:45:54 -0400

    added comment to explain addition
    code is efficient but confusing

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,8 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + b },
+    //gotta have plus in front of a and b
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
